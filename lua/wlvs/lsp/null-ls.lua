2348dec7e88c663e9d9726d6f3eb41714d69464e
Wolves <cstingl@protonmail.com>
2021-12-21 17:24:41 -0500

    Add lua stylua and selene to lsp

--- lua/wlvs/lsp/null-ls.lua
+++ lua/wlvs/lsp/null-ls.lua
@@ -1,31 +1,31 @@
 local M = {}
 
 function M.setup(options)
-  local status_ok, nls = pcall(require, "null-ls")
-  if not status_ok then
-    return
-  end
+	local status_ok, nls = pcall(require, "null-ls")
+	if not status_ok then
+		return
+	end
 
-  nls.setup({
-    debounce = 150,
-    save_after_format = false,
-    sources = {
-      nls.builtins.formatting.prettierd,
-      nls.builtins.formatting.stylua,
-      nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
-      nls.builtins.formatting.eslint_d,
-      nls.builtins.diagnostics.markdownlint,
-      -- nls.builtins.diagnostics.selene,
-    },
-    on_attach = options.on_attach,
-    root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".nvim.settings.json", ".git"),
-  })
+	nls.setup({
+		debounce = 150,
+		save_after_format = false,
+		sources = {
+			nls.builtins.formatting.prettierd,
+			nls.builtins.formatting.stylua,
+			nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
+			nls.builtins.formatting.eslint_d,
+			nls.builtins.diagnostics.markdownlint,
+			nls.builtins.diagnostics.selene,
+		},
+		on_attach = options.on_attach,
+		root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".nvim.settings.json", ".git"),
+	})
 end
 
 function M.has_formatter(ft)
-  local sources = require("null-ls.sources")
-  local available = sources.get_available(ft, "NULL_LS_FORMATTING")
-  return #available > 0
+	local sources = require("null-ls.sources")
+	local available = sources.get_available(ft, "NULL_LS_FORMATTING")
+	return #available > 0
 end
 
 return M
