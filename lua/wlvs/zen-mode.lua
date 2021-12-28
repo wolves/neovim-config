5e0f632e3dcc52a2f06e6cd772f89c6996dae992
Wolves <cstingl@protonmail.com>
2021-12-28 11:33:00 -0500

    Adjust zen-mode and VimPencil

--- lua/wlvs/zen-mode.lua
+++ lua/wlvs/zen-mode.lua
@@ -6,8 +6,8 @@ end
 zen_mode.setup({
   window = {
     backdrop = 0.8,
-    height = 0.9, -- height of the Zen window
-    width = 0.85,
+    height = 1, -- height of the Zen window
+    width = 100,
     options = {
       signcolumn = "no", -- disable signcolumn
       number = false, -- disable number column
@@ -23,6 +23,14 @@ zen_mode.setup({
     tmux = { enabled = false },
     twilight = { enabled = true },
   },
+  -- callback where you can add custom code when the Zen window opens
+  on_open = function()
+    vim.cmd("PencilSoft")
+  end,
+  -- callback where you can add custom code when the Zen window closes
+  on_close = function()
+    vim.cmd("PencilOff")
+  end,
   -- on_open = function()
   --   vim.lsp.diagnostic.disable()
   --   vim.cmd [[
